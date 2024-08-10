f3ee7954a50c2c6eed8c60eed5e07c50a991b474
Cindy Le <cindyle106@gmail.com>
2024-08-10 08:42:08 -0600

    added README

--- js/app.js
+++ js/app.js
@@ -111,6 +111,11 @@ resetBtnEl.addEventListener('click',init)
 init();
 
 
-
+//create a widget for the game 
+//add sound unit one level up (audio)
+// add different emojis for each one 
+// move the hello kitty up to the middle of the screen using flexbox 
+// create a popout with instructions with how to play
+// 
 
 
